351e9ab3b05e0321e20e913eed9b5c38a6c61427
Rasel Ahmed <raselahmed63840@gmail.com>
2026-01-21 21:59:43 +0600

    Old version Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Movie } from "./pages/Movie";
 import { Contact } from "./pages/Contact";
+import { About } from "./pages/About";
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/movie",
-      element: <Movie />,
-    },
-    {
-      path: "/contact",
-      element: <Contact />,
-    },
-  ]);
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Home />,
+  //   },
+  //   {
+  //     path: "/movie",
+  //     element: <Movie />,
+  //   },
+  //   {
+  //     path: "/contact",
+  //     element: <Contact />,
+  //   },
+  // ]);
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/movie" element={<Movie />} />
+        <Route path="/contact" element={<Contact />} />
+      </Route>,
+    ),
+  );
 
   return <RouterProvider router={router} />;
 };
