274fec33212731e5b4d3bbff7046f25f86db46d2
Rasel Ahmed <raselahmed63840@gmail.com>
2026-01-21 22:15:57 +0600

    react outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,42 @@
 import React from "react";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Movie } from "./pages/Movie";
 import { Contact } from "./pages/Contact";
-import { About } from "./pages/About";
-const App = () => {
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <Home />,
-  //   },
-  //   {
-  //     path: "/movie",
-  //     element: <Movie />,
-  //   },
-  //   {
-  //     path: "/contact",
-  //     element: <Contact />,
-  //   },
-  // ]);
+import AppLayout from "./component/layout/AppLayout";
 
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/movie" element={<Movie />} />
-        <Route path="/contact" element={<Contact />} />
-      </Route>,
-    ),
-  );
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/home",
+          element: <Home />,
+        },
+        {
+          path: "/movie",
+          element: <Movie />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+      ],
+    },
+  ]);
+
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route>
+  //       <Route path="/" element={<Home />} />
+  //       <Route path="/about" element={<About />} />
+  //       <Route path="/movie" element={<Movie />} />
+  //       <Route path="/contact" element={<Contact />} />
+  //     </Route>,
+  //   ),
+  // );
 
   return <RouterProvider router={router} />;
 };
---
+++ src/component/layout/AppLayout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Footer } from "./Footer";
+import Header from "./Header";
+
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default AppLayout;
